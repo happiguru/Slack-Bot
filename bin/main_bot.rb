b58a56699084cdb94cf322bf06af3498d0d9ca6f
happiguru <hguruman@gmail.com>
2020-09-11 21:46:41 +0400

    Add methods to read the instructions from json file upon joining slack team

--- bin/main_bot.rb
+++ bin/main_bot.rb
@@ -1,3 +1,22 @@
-require '../lib/auth.rb'
+require 'sinatra/base'
+require 'slack-ruby-client'
 
-Bot.run
+class SlackBot
+    def self.welcome_text
+        "Welcome to Slack Bot Team! Glad to have you! Please complete the steps below"
+    end
+
+    def self.slack_json
+        instruct_file = File.read('../initialize.json')
+        instruct_json = JSON.parse(instruct_file)
+        attachments = instruct_json['attachments']
+    end
+
+    def self.items
+        { reaction: 0, pin: 1, share: 2 }
+    end
+
+    def self.new
+        instruct_json.deep_dup
+    end
+end
