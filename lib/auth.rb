e41a573fcc95850ae0b65353645f1bf1a6c5d147
happiguru <hguruman@gmail.com>
2020-09-11 20:56:06 +0400

    Add slack_config hash and a check to test if parameters are sent

--- lib/auth.rb
+++ lib/auth.rb
@@ -2,4 +2,21 @@
 require 'sinatra/base'
 require 'slack-ruby-bot'
 require 'slack-ruby-client'
+require_relative '../env.rb'
 
+SLACK_CONFIG = {
+    slack_client_id: SLACK_CLIENT_ID,
+    slack_api_secret: SLACK_API_SECRET,
+    slack_redirect_uri: SLACK_REDIRECT_URI,
+    slack_verification_token: SLACK_VERIFICATION_TOKEN
+}.freeze
+
+#Check to see if required variables were provided
+missing_params = SLACK_CONFIG.select { |_key, value| value.nil? }
+if missing_params.any?
+    error_message = missing_params.keys.join(', ').upcase
+    raise "Missing Slack config variables: #{error_message}"
+end
+
+# Set OAuth Scope for the Slack-Bot
+BOT_SCOPE = 'bot'.freeze
