1bcba8ef139fa591f31f80e13e417ea2f60b7953
happiguru <hguruman@gmail.com>
2020-09-11 21:32:06 +0400

    Update auth file

--- lib/auth.rb
+++ lib/auth.rb
@@ -32,3 +32,49 @@ def create_slack_cleint(slack_api_secret)
     end
     Slack::Web::Client.new
 end
+
+class Auth < Sinatra::base
+    add_to_slack_button = %(
+        <a href="https://slack.com/oauth/v2/authorize?user_scope=#{BOT_SCOPE}&client_id=#{SLACK_CONFIG[:slack_client_id]}"><img src="https://api.slack.com/img/sign_in_with_slack.png" /></a>
+    )
+
+    get '/' do
+        redirect '/begin_auth'
+    end
+
+    get '/begin_auth' do
+        status 200
+        body add_to_slack_button
+    end
+
+    get '/finish_auth' do
+        client = Slack::Web::Client.new
+        begin
+            response = client.oauth_access(
+                {
+                    client_id: SLACK_CONFIG[:slack_client_id],
+                    client_secret: SLACK_CONFIG[:slack_api_secret],
+                    redirect_uri: SLACK_CONFIG[:slack_redirect_uri],
+                    code: params[:code]
+                }
+            )
+
+            team_id = response['team_id']
+            $teams[team_id] = {
+                user_access_token: response['access_token'],
+                bot_user_id: response['bot']['bot_user_id'],
+                bot_access_token: response['bot']['bot_access_token']
+            }
+
+            $teams[team_id]['client'] = create_slack_cleint(response['bot']['bot_access_token'])
+
+            status 200
+            body 'Gloraay! Authentication succeeded!'
+        rescue Slack::Web::Api:;Error => e
+
+            status 403
+            body "Oooops! Authentication failed! Reason: #{e.message}<br/>#{add_to_slack_button}"
+        end
+    end
+end
+
