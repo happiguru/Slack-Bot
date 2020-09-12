ace75a2ae6885b0eae61fa5596e5a90a3c65d2dd
happiguru <hguruman@gmail.com>
2020-09-12 12:46:00 +0400

    Fix linters error

--- bin/main_bot.rb
+++ bin/main_bot.rb
@@ -9,8 +9,8 @@ class SlackBot
   def self.slack_json
     instruct_file = File.read('../initialize.json')
     instruct_json = JSON.parse(instruct_file)
-    #instruct_json['attachments']
-    attachments = instruct_json['attachments']
+    instruct_json['attachments']
+    # attachments = instruct_json['attachments']
   end
 
   def self.items
@@ -29,101 +29,103 @@ class SlackBot
 end
 
 class API < Sinatra::Base
-    post '/events' do
-        request_data = JSON.parse(request.body.read)
+  post '/events' do
+    request_data = JSON.parse(request.body.read)
 
-        unless SLACK_CONFIG[:slack_verification_token] == request_data['token']
-            halt 403, "Invalid Slack verification token received: #{request_data['token']}"
-        end
-
-        case request_data['type']
-        when 'url_verification'
-            request_data['challenge']
-
-        when 'event_callback'
-            team_id = request_data['team_id']
-            event_data = request_data['event']
-
-            case event_data['type']
-            when 'team_join'
-                Events.user_join(team_id, event_data)
-            when 'reaction_added'
-            
-            when 'pin_added'
-                Events.pin_added(team_id, event_data)
-
-            when 'message'
-                Events.message(team_id, event_data)
-
-            else
-                puts "Unexpected event:\n"
-                puts JSON.pretty_generate(request_data)
-            end
-            status 200
-        end
+    unless SLACK_CONFIG[:slack_verification_token] == request_data['token']
+      halt 403, "Invalid Slack verification token received: #{request_data['token']}"
     end
+
+    case request_data['type']
+    when 'url_verification'
+      request_data['challenge']
+
+    when 'event_callback'
+      team_id = request_data['team_id']
+      event_data = request_data['event']
+
+      case event_data['type']
+      when 'team_join'
+        Events.user_join(team_id, event_data)
+
+      when 'reaction_added'
+        Events.reaction_added(team_id, event_data)
+
+      when 'pin_added'
+        Events.pin_added(team_id, event_data)
+
+      when 'message'
+        Events.message(team_id, event_data)
+
+      else
+        puts "Unexpected event:\n"
+        puts JSON.pretty_generate(request_data)
+      end
+      status 200
+    end
+  end
 end
 
 class Events
-    def self.user_join(team_id, event_data)
-        user_id = event_data['user']['id']
-        
-        teams[team_id][user_id] = {
-            bot_content: SlackBot.new
-        }
-        send_response(team_id, user_id)
-    end
+  def self.user_join(team_id, event_data)
+    user_id = event_data['user']['id']
 
-    def self.reaction_added(team_id, event_data)
+    teams[team_id][user_id] = {
+      bot_content: SlackBot.new
+    }
+    send_response(team_id, user_id)
+  end
+
+  def self.reaction_added(team_id, event_data)
+    user_id = event_data['user']
+    if teams[team_id][user_id]
+      channel = event_data['item']['channel']
+      ts = event_data['item']['ts']
+      SlackBot.update_i(team_id, user_id, SlackBot.items[:reaction])
+      send_response(team_id, user_id, channel, ts)
+    end
+  end
+
+  def self.pin_added(team_id, event_data)
+    user_id = event_data['user']
+    if teams[team_id][user_id]
+      channel = event_data['item']['channel']
+      ts = event_data['item']['message']['ts']
+      SlackBot.update_i(team_id, user_id, SlackBot.items[:pin])
+      send_response(team_id, user_id, channel, ts)
+    end
+  end
+
+  def self.message(team_id, event_data)
+    user_id = event_data['user']
+    unless user_id == teams[team_id][:bot_user_id]
+
+      if event_data['attachments'] && event_data['attachments'].first['is_share']
         user_id = event_data['user']
-        if teams[team_id][user_id]
-            channel = event_data['item']['channel']
-            ts = event_data['item']['ts']
-            SlackBot.update_i(team_id, user_id, SlackBot.items[:reaction])
-            send_response(team_id, user_id, channel, ts)
-        end
+        ts = event_data['attachments'].first['ts']
+        channel = event_data['channel']
+        SlackBot.update_i(team_id, user_id, SlackBot.items[:share])
+        send_response(team_id, user_id, channel, ts)
+      end
     end
+  end
 
-    def self.pin_added(team_id, event_data)
-        user_id = event_data['user']
-        if teams[team_id][user_id]
-            channel = event_data['item']['channel']
-            ts = event_data['item']['message']['ts']
-            SlackBot.update_i(team_id, user_id, SlackBot.items[:pin])
-            send_response(team_id, user_id, channel, ts)
-        end
+  def self.send_response(team_id, user_id, channel = user_id, time_stamp = nil)
+    if ts
+      teams[team_id]['client'].chat_update(
+        as_user: 'true',
+        channel: channel,
+        time_stamp: time_stamp,
+        text: SlackBot.welcome_text,
+        attachments: teams[team_id][user_id][:bot_content]
+      )
+    else
+      teams[team_id]['client'].chat_postMessage(
+        as_user: 'true',
+        channel: channel,
+        text: SlackBot.welcome_text,
+        attachments: teams[team_id][user_id][:bot_content]
+      )
     end
-
-    def self.message(team_id, event_data)
-        user_id = event_data['user']
-        unless user_id == teams[team_id][:bot_user_id]
-
-            if event_data['attachments'] && event_data['attachments'].first['is_share']
-                user_id = event_data['user']
-                ts = event_data['attachments'].first['ts']
-                channel = event_data['channel']
-                SlackBot.update_i(team_id, user_id, SlackBot.items[:share])
-                send_response(team_id, user_id, channel, ts)
-            end
-        end
-    end
-
-    def self.send_response(team_id, user_id, channel = user_id, ts = nil)
-        if ts
-            teams[team_id]['client'].chat_update(
-                as_user: 'true',
-                channel: channel,
-                ts: ts,
-                text: SlackBot.welcome_text,
-                attachments: teams[team_id][user_id][:bot_content]
-            )
-        else
-            teams[team_id]['client'].chat_postMessage(
-                as_user: 'true',
-                channel: channel,
-                text: SlackBot.welcome_text,
-                attachments: teams[team_id][user_id][:bot_content]
-            )
-        end
-    end
+  end
 end
