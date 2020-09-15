52f25f079492ed1c99e5c2c6c3a0b3950c10839c
hguruman <hguruman@gmail.com>
2020-09-15 21:04:13 +0400

    Update auth file

--- config/env.rb
+++ config/env.rb
@@ -2,6 +2,6 @@
 
 SLACK_API_TOKEN = 'xoxb-1341131635942-1371806137072-mMv2rmaKF60hzHylmFoN76wj'
 
-    Slack.configure do |config|
-    config.Token = ENV['SLACK_API_TOKEN']
-    end
+    # Slack.configure do |config|
+    # config.Token = ENV['SLACK_API_TOKEN']
+    # end
