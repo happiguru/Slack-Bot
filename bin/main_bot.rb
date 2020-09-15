e9d290c22fbe9e65fec5cda13a9d9d414eb4c447
Stanley Enow Lekunze <hguruman@gmail.com>
2020-09-15 18:55:15 +0400

    Update README file for instructions to install the app

--- bin/main_bot.rb
+++ bin/main_bot.rb
@@ -13,21 +13,21 @@ class SlackBot
     @index_item = index_item
   end
 
+  def self.welcome_text
+    'Welcome to Slack Bot Team! Glad to have you! Please complete the steps below'
+  end
+
   def self.update_i(team_id, user_id, index_item)
     bot_item = team[team_id][user_id][:bot_content][index_item]
     bot_item['text'].sub!(':white_large_square:', ':white_check_mark:')
     bot_item['color'] = '#F47FF1'
   end
 
-  def self.welcome_text
-    'Welcome to Slack Bot Team! Glad to have you! Please complete the steps below'
-  end
-
-  def self.slack_json
-    instruct_file = File.read('../initialize.json')
-    slack_json = JSON.parse(instruct_file)
-    slack_json['attachments']
-    # attachments = slack_json['attachments']
+  def self.slackbot_json
+    slackbot_file = File.read('../initialize.json')
+    slackbot_json = JSON.parse(slackbot_file)
+    slackbot_json['attachments']
+    # attachments = slackbot_json['attachments']
   end
 
   def self.items
