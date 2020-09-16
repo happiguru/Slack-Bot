19c9f1e399ead2a3792e01739ab60f1628f2d823
hguruman <hguruman@gmail.com>
2020-09-16 20:01:43 +0400

    Fix linter errors

--- spec/main_bot_spec.rb
+++ spec/main_bot_spec.rb
@@ -3,8 +3,8 @@ require_relative '../bin/main_bot.rb'
 describe SlackBot do
   describe '#welcome_text' do
     it 'returns a welcome message and gives instruction' do
-    s = SlackBot.new
-    expect(s.welcome_text).to eql 'Welcome to Slack Bot Team! Glad to have you! Please complete the steps below'
+      s = SlackBot.new
+      expect(s.welcome_text).to eql 'Welcome to Slack Bot Team! Glad to have you! Please complete the steps below'
     end
   end
 end
