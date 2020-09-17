eb86bf80127dac57dd5280a9f5d646ab0a1dc7ad
Stanley Enow Lekunze <hguruman@gmail.com>
2020-09-17 05:51:25 +0400

    Add spec for slackbot_json method

--- spec/main_bot_spec.rb
+++ spec/main_bot_spec.rb
@@ -7,4 +7,11 @@ describe SlackBot do
       expect(s.welcome_text).to eql 'Welcome to Slack Bot Team! Glad to have you! Please complete the steps below'
     end
   end
+
+  describe '#slackbot_json' do
+    it 'sends an array containing the message in initialized.json file' do
+      s = SlackBot.new
+      expect(s.slackbot_json).to be_truthy
+    end
+  end
 end
