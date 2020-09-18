be75d0e049fc2e2bf004ed6898aa27acb31ca277
Stanley Enow Lekunze <hguruman@gmail.com>
2020-09-18 10:35:33 +0400

    remove from gitignore

--- spec/events_spec.rb
+++ spec/events_spec.rb
@@ -1,15 +1,13 @@
-# require_relative '../lib/events.rb'
+require_relative '../lib/events.rb'
 
-# describe Events do
-#   describe '#user_join' do
-#     it 'send a response to update a channel when a new user joins a channel' do
-#       s = Events.new
-#       team_id = 1234
-#       event_data = 'team_join'
-#       user = 'USER'
-#       id = 1212
-#       joined = s.user_join(team_id, event_data)
-#       expect(joined).to be_truthy
-#     end
-#   end
-# end
+describe Events do
+  describe '#user_join' do
+    it 'send a response to update a channel when a new user joins a channel' do
+      s = Events.new
+      team_id = 1234
+      event_data = 'team_join'
+      joined = s.user_join(team_id, event_data)
+      expect(joined).to be_truthy
+    end
+  end
+end
